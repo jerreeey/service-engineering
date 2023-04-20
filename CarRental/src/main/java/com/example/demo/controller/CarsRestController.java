daddac5a4f3223dbcb0822865c4a09d640573804
goldis <goldis.salarmaaf@hotmail.com>
2023-04-20 22:41:01 +0200

    CarRestController without Currency Converter

--- CarRental/src/main/java/com/example/demo/controller/CarsRestController.java
+++ CarRental/src/main/java/com/example/demo/controller/CarsRestController.java
@@ -3,8 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.DTO.CarsDTO;
 import com.example.demo.service.CarsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -15,8 +20,8 @@ public class CarsRestController {
     CarsService carsService;
 
     @GetMapping
-    public List<CarsDTO> findUser(@RequestParam String pickupdate, @RequestParam String pickuphour, @RequestParam String returndate, @RequestParam String returnhour, @RequestParam String currency) {
+    public ResponseEntity<List<CarsDTO>> findCars(@RequestParam String pickupdate, @RequestParam String pickuphour, @RequestParam String returndate, @RequestParam String returnhour, @RequestParam String currency) throws ParseException {
         //return all cats with selected currency where no booking is between pickup and return + hours
-        return carsService.findCars(pickupdate, pickuphour, returndate, returnhour, currency);
+        return ResponseEntity.ok(carsService.findCars(pickupdate, pickuphour, returndate, returnhour, currency));
     }
 }
--- CarRental/src/main/java/com/example/demo/repository/CarsRepository.java
+++ CarRental/src/main/java/com/example/demo/repository/CarsRepository.java
@@ -9,10 +9,4 @@ import java.util.List;
 
 @Repository
 public interface CarsRepository extends JpaRepository<Cars, Integer> {
-
-    @Query(value = "SELECT c.make, c.model, c.year, c.currency, c.daily_rate, b.start_date, b.end_date, b.returned, c.car_id as id, b.car_id as car_id FROM Bookings b LEFT JOIN Cars c ON c.car_id = b.car_id",
-            nativeQuery = true)
-    List<Cars> getAllCars();
-
-    //todo filter?
 }
