4e1d594cf9d5f7534958a42a98dd6d1dccf02211
PeclBoi <alexander.pecl@gmx.at>
2023-04-29 11:05:04 +0200

    Added Basic Authentication using SOAP header

--- CurrencyConverter/Service/CurrencyConverterWS.asmx.cs
+++ CurrencyConverter/Service/CurrencyConverterWS.asmx.cs
@@ -1,7 +1,9 @@
 ﻿using CurrencyConverterWebServiceTests;
 using Service.CurrencyConverterLogic;
 using System.Net.Http;
+using System.Security.Authentication;
 using System.Web.Services;
+using System.Web.Services.Protocols;
 
 namespace Service
 {
@@ -9,7 +11,7 @@ namespace Service
     /// Summary description for WebService1
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [WebServiceBinding(Name = "TestService", ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
@@ -17,10 +19,20 @@ namespace Service
     {
 
 
+        public UserCredentials consumer;
+
 
         [WebMethod]
+        [SoapDocumentMethod(Binding = "TestService")]
+        [SoapHeader("consumer")]
         public decimal ConvertCurrency(decimal amount, string from, string to)
         {
+
+            if (!checkConsumer())
+            {
+                throw new AuthenticationException("Fehler bei der Authentifizierung: ungültige Anmeldeinformationen.");
+            }
+
 
             IHttpClient httpClient = new HttpClientWrapper();
             CurrencyConverter currencyConverter = new CurrencyConverter();
@@ -35,5 +47,23 @@ namespace Service
             return currencyConverter.Convert(amount, fromRate, toRate);
 
         }
+
+
+        private bool checkConsumer()
+        {
+            // In this method you can check the username and password 
+            // with your database or something
+            // You could also encrypt the password for more security
+            if (consumer != null)
+            {
+                if (consumer.username == "Alex" && consumer.password == "1234")
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
     }
 }
